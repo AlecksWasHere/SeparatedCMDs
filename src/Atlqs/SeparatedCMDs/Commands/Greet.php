8829b018e61e6cd5792b03b71f944ae20b3bc40d
Al33eXx <52435658+AlecksWasHere@users.noreply.github.com>
2021-05-03 18:02:42 +0400

    Create Greet.php
    Creating the /greet command.

---
+++ src/Atlqs/SeparatedCMDs/Commands/Greet.php
@@ -0,0 +1,37 @@
+<?php /** @noinspection PhpUnused */
+declare(strict_types=1);
+
+namespace Atlqs\SeparatedCMDs\Commands;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\PluginCommand;
+use pocketmine\Player;
+use pocketmine\plugin\Plugin;
+use pocketmine\utils\TextFormat;
+
+class Greet extends PluginCommand {
+    public function __construct(Plugin $plugin) {
+        parent::__construct("greet", $plugin);
+        $this->setPermission("greet.separatedcmds");
+        $this->setDescription("Greeting command");
+    }
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$sender->hasPermission($this->getPermission())) {
+            $sender->sendMessage(TextFormat::RED . "You do not have permissions to run this command.");
+        }
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("Please run the command in-game.");
+        }
+        switch ($args[0] ?? "greet") {
+            case "greet":
+                if (!$sender->hasPermission("greet.separatedcmds")) {
+                    $sender->sendMessage("Use this command in-game.");
+                } else {
+                    //What the command does here.
+                    $name = $sender->getName();
+                    $sender->sendMessage(TextFormat::BLUE . "Hello $name!");
+                }
+        }
+        return true;
+    }
+}
